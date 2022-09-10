a6399e1ec5629ef35bc4c80897a11edb9952b0d9
kartikeysaran <sarankartikey@gmail.com>
2022-09-10 12:39:34 +0530

    Added NGO Dashboard and Some minor UI Change

---
+++ app/src/main/java/ks/app/foodilize/Ngo_Dashboard.java
@@ -0,0 +1,45 @@
+package ks.app.foodilize;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.tabs.TabLayout;
+
+import androidx.viewpager.widget.ViewPager;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import ks.app.foodilize.ui.main.SectionsPagerAdapter;
+import ks.app.foodilize.databinding.ActivityNgoDashboardBinding;
+
+public class Ngo_Dashboard extends AppCompatActivity {
+
+    private ActivityNgoDashboardBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityNgoDashboardBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
+        ViewPager viewPager = binding.viewPager;
+        viewPager.setAdapter(sectionsPagerAdapter);
+        TabLayout tabs = binding.tabs;
+        tabs.setupWithViewPager(viewPager);
+        FloatingActionButton fab = binding.fab;
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+}
