487ae4b661cafcf6e86e7d16af93e01149fb50a8
kartikeysaran <sarankartikey@gmail.com>
2022-09-10 12:10:38 +0530

    Initial commit

---
+++ app/src/main/java/ks/app/foodilize/MainActivity.java
@@ -0,0 +1,14 @@
+package ks.app.foodilize;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
